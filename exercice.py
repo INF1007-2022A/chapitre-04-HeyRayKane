247cc06c14c739d7172e05bf394efb79dbcb5461
HeyRayKane <rayan.ajakane@polymtl.ca>
2022-09-29 20:38:02 -0400

    Complétion des exercices du chapitre 4

--- exercice.py
+++ exercice.py
@@ -3,24 +3,43 @@
 
 
 def is_even_len(string: str) -> bool:
-    pass
-
+    pair = len(string)%2 == 0
+    return pair
 
 def remove_third_char(string: str) -> str:
-    pass
+    new_string = string[:2]+string[3:]
+    return new_string
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    pass
+    new_string = string.replace(old_char,new_char)
+    return new_string
+
+def replace_char_2(string: str, old_char: str, new_char: str) -> str:
+    for i in range(len(string)):
+        if string[i] == old_char:
+            new_string = string[:i] + new_char + string[i+1:]
+    return new_string
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    pass
-
+    n=0
+    for i in string:
+        if char == i:
+            n+=1
+    return n
 
 def get_number_of_words(sentence: str, word: str) -> int:
-    pass
+    number= sentence.count(word)
+    return number
 
+def get_number_of_words_2(sentence: str, word: str) -> int:
+    n=0
+    a=sentence.split(" ")
+    for i in a:
+        if word in i:
+            n+=1
+    return n
 
 def main() -> None:
     chaine = "Bonjour!"
@@ -34,12 +53,15 @@ def main() -> None:
 
     chaine = "hello world!"
     print(f"On remplace le caratère w par le caractère z dans la chaine: {chaine}. Résultat : {replace_char(chaine, 'w', 'z')}")
+    print(f"On remplace le caratère w par le caractère z dans la chaine: {chaine}. Résultat : {replace_char_2(chaine, 'w', 'z')}")
 
     print(f"Le nombre d'occurrence de l dans {chaine} est : {get_number_of_char(chaine, 'l')}")
     
-    chaine = "Baby shark doo doo doo doo doo doo"
+    chaine = "Baby shark doo, doo doo doo doo doo"
     print(f"L'occurence du mot doo dans la chaine {chaine} est: {get_number_of_words(chaine, 'doo')}")
 
+    chaine = "Baby shark doo, doo doo doo doo doo"
+    print(f"L'occurence du mot doo dans la chaine {chaine} est: {get_number_of_words_2(chaine, 'doo')}")
 
 if __name__ == '__main__':
     main()
